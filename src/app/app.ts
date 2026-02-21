e3d001168eacaf6dbc080051bbcfdc72d87b20a4
dububear15 <raphoncele@gmail.com>
2026-02-21 08:16:06 +0800

    Lab 3 - Dynamic products with Angular

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,13 +1,27 @@
-import { Component, signal } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { Products } from './products/products';
+import { CommonModule } from '@angular/common'; // This gives you pipes like | currency
+import { Products } from './products/products'; // Ensure this matches your folder exactly
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Products],
+  standalone: true, // Make sure this is here for 'imports' to work
+  imports: [RouterOutlet, Products, CommonModule], // Error should vanish now
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
+
+// src/app/app.ts
 export class App {
-  protected readonly title = ('my-first-angular-shop');
+  title = 'my-first-angular-shop';
+  cartItems: any[] = []; // Holds your items
+
+  // This receives the product from the button
+  onItemAdded(product: any) {
+    this.cartItems.push(product); 
+  }
+
+  getTotal(): number {
+    return this.cartItems.reduce((acc, item) => acc + item.price, 0);
+  }
 }
